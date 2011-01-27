b4e8a60e19c7dfec5c37286445ca3e9c37678a87
Mikhail Mangushev <mishamx@gmail.com>
2011-01-27 15:59:10 +0000

    Portuguese translation (fixed issue 51)

---
+++ modules/user/messages/pt/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Nome do modelo',
+	'Lable field name'=>'Nome do campo',
+	'Empty item name'=>'Nome de item vazio',
+	'Profile model relation name'=>'Modelo de perfil com relação ao nome',
+);
