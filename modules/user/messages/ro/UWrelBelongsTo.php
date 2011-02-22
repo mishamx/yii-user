3122b06041c1c4ee30dc284349f7b62bde0b415b
Mikhail Mangushev <mishamx@gmail.com>
2011-02-22 09:46:33 +0000

    Added Romanian translation (thanks @matricks)

---
+++ modules/user/messages/ro/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Nume model',
+	'Lable field name'=>'Eticheta nume camp',
+	'Empty item name'=>'Nume element gol',
+	'Profile model relation name'=>'Denumire model relational profil',
+);
