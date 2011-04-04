fb4d6e74294d2050f6a7e4a0820c25045527986a
Mikhail Mangushev <mishamx@gmail.com>
2011-04-04 12:06:36 +0000

    Polish translation added (thanks aleksand...@gmail.com) Fixed issue 79

---
+++ modules/user/messages/pl/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Nazwa modelu',
+	'Lable field name'=>'Nazwa etykiety pola',
+	'Empty item name'=>'Nazwa pustego elementu',
+	'Profile model relation name'=>'Nazwa profilu modelu',
+);
