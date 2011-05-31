874bab19de8ccab13265bd0a0f295a1f2bfba58d
Mikhail Mangushev <mishamx@gmail.com>
2011-05-31 09:03:41 +0000

    Added dutch translation (thanks afj.lamb...@gmail.com) Fixed issue 86

---
+++ modules/user/messages/nl/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Modelnaam',
+	'Lable field name'=>'Lable veldnamen',
+	'Empty item name'=>'Leeg item-naam',
+	'Profile model relation name'=>'Profiel model relatienaam',
+);
