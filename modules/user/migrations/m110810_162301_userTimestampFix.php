b96ee58c27ad85b019d1215e1a5bbc77536d9e3f
Mikhail Mangushev <mishamx@gmail.com>
2011-08-11 13:52:36 +0400

    Use local setting for migration

--- modules/user/migrations/m110810_162301_userTimestampFix.php
+++ modules/user/migrations/m110810_162301_userTimestampFix.php
@@ -4,19 +4,32 @@ class m110810_162301_userTimestampFix extends CDbMigration
 {
 	public function safeUp()
 	{
-        $this->addColumn('{{user}}','create_at',"TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
-        $this->addColumn('{{user}}','lastvisit_at',"TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        $this->execute("UPDATE {{user}} SET create_at = FROM_UNIXTIME(createtime), lastvisit_at = FROM_UNIXTIME(lastvisit)");
-        $this->dropColumn('{{user}}','createtime');
-        $this->dropColumn('{{user}}','lastvisit');
+        if (!Yii::app()->getModule('user')) {
+            echo "\n\nAdd to console.php :\n"
+                 ."'modules'=>array(\n"
+                 ."...\n"
+                 ."    'user'=>array(\n"
+                 ."        ... # copy settings from main config\n"
+                 ."    ),\n"
+                 ."...\n"
+                 ."),\n"
+                 ."\n";
+            return false;
+        }
+
+        $this->addColumn(Yii::app()->getModule('user')->tableUsers,'create_at',"TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        $this->addColumn(Yii::app()->getModule('user')->tableUsers,'lastvisit_at',"TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00'");
+        $this->execute("UPDATE {{users}} SET create_at = FROM_UNIXTIME(createtime), lastvisit_at = FROM_UNIXTIME(lastvisit)");
+        $this->dropColumn(Yii::app()->getModule('user')->tableUsers,'createtime');
+        $this->dropColumn(Yii::app()->getModule('user')->tableUsers,'lastvisit');
 	}
 
 	public function safeDown()
 	{
-        $this->addColumn('{{user}}','createtime',"int(10) NOT NULL");
-        $this->addColumn('{{user}}','lastvisit',"int(10) NOT NULL");
-        $this->execute("UPDATE {{user}} SET createtime = UNIX_TIMESTAMP(create_at), lastvisit = UNIX_TIMESTAMP(lastvisit_at)");
-        $this->dropColumn('{{user}}','create_at');
-        $this->dropColumn('{{user}}','lastvisit_at');
+        $this->addColumn(Yii::app()->getModule('user')->tableUsers,'createtime',"int(10) NOT NULL");
+        $this->addColumn(Yii::app()->getModule('user')->tableUsers,'lastvisit',"int(10) NOT NULL");
+        $this->execute("UPDATE {{users}} SET createtime = UNIX_TIMESTAMP(create_at), lastvisit = UNIX_TIMESTAMP(lastvisit_at)");
+        $this->dropColumn(Yii::app()->getModule('user')->tableUsers,'create_at');
+        $this->dropColumn(Yii::app()->getModule('user')->tableUsers,'lastvisit_at');
 	}
 }
