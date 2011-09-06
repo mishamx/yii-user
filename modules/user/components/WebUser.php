3ea50cec2c73d89b27e1494f2076a2b34412258b
Mikhail Mangushev <mishamx@gmail.com>
2011-09-06 14:52:53 +0400

    WebUser: comment stat module Migration: added current time

--- modules/user/components/WebUser.php
+++ modules/user/components/WebUser.php
@@ -13,19 +13,19 @@ class WebUser extends CWebUser
         return $this->getState('__id') ? $this->getState('__id') : 0;
     }
 
-    protected function beforeLogin($id, $states, $fromCookie)
-    {
-        parent::beforeLogin($id, $states, $fromCookie);
-
-        $model = new UserLoginStats();
-        $model->attributes = array(
-            'user_id' => $id,
-            'ip' => ip2long(Yii::app()->request->getUserHostAddress())
-        );
-        $model->save();
-
-        return true;
-    }
+//    protected function beforeLogin($id, $states, $fromCookie)
+//    {
+//        parent::beforeLogin($id, $states, $fromCookie);
+//
+//        $model = new UserLoginStats();
+//        $model->attributes = array(
+//            'user_id' => $id,
+//            'ip' => ip2long(Yii::app()->request->getUserHostAddress())
+//        );
+//        $model->save();
+//
+//        return true;
+//    }
 
     protected function afterLogin($fromCookie)
 	{
