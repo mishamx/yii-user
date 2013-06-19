852c4b0f76f8f26b04c30b9477d2d8d53dd12d98
prawee wongsa <prawee@hotmail.com>
2013-06-19 19:49:57 +0700

    Update LoginController.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends Controller
 	
 	private function lastViset() {
 		$lastVisit = User::model()->notsafe()->findByPk(Yii::app()->user->id);
-		$lastVisit->lastvisit_at = new CDbExpression('NOW()');
+		$lastVisit->lastvisit = new CDbExpression('NOW()');
 		$lastVisit->save();
 	}
 
