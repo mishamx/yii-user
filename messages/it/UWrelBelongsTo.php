5286816eca5682e19e585cdead30cbec6817033e
Denis Seghetti <info@it-serv.it>
2014-01-03 12:47:36 +0100

    Italian translation files

---
+++ messages/it/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Nome del Modello',
+	'Lable field name'=>'Etichetta Campo Nome',
+	'Empty item name'=>'Campo Nome Vuoto',
+	'Profile model relation name'=>'Nome della Relazione con il Profilo',
+);
